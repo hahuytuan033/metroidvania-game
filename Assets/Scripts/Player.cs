32c3f53602a392caa93c89f1dd03661fa372fb3a
Huy Tuấn <163986199+hahuytuan033@users.noreply.github.com>
2025-01-13 14:28:58 +0700

    remove move the player with input system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,26 +1,7 @@
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-   public Rigidbody2D rb;
-   [SerializeField] private float speed = 5f;
-
-   float horizontalMovement;
-
-   void Start()
-   {
-      rb = GetComponent<Rigidbody2D>();
-   }
-
-   void Update()
-   {
-      horizontalMovement = Input.GetAxis("Horizontal");
-   }
-
-    public void Move(InputAction.CallbackContext context)
-    {
-        horizontalMovement = context.ReadValue<float>();
-    }
+   
 }
