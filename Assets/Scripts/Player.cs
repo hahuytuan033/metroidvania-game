25ed05b8b041745dec3700a509821a1ea55c066d
Huy Tuấn <163986199+hahuytuan033@users.noreply.github.com>
2025-01-13 14:46:03 +0700

    move to mouse

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,5 +3,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-   
+   Vector2 difference= Vector2.zero;
+
+   private void OnMouseDown()
+   {
+      difference = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)transform.position;
+   }
+
+   private void OnMouseDrag()
+   {
+      transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - difference;
+   }
 }
